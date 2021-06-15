61c1cac2ae93571df077e40cf649758e2109312a
aiontrust <85944803+aiontrust@users.noreply.github.com>
2021-06-15 11:30:13 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using CoinTipsApp.Services;
+using ZXing.PDF417.Internal;
+using System.Windows.Input;
+
+namespace CoinTipsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+               
+        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQrScanningService>();
+                var result = await scanner.ScanAsync();
+                if (result != null)
+                {
+                    txtBarcode.Text = result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+                  
+        private void xferButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
